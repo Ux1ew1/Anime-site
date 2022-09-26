9011d65f6445bac6760a84e2e0f660895fa8624f
Ux1ew1 <105740400+Ux1ew1@users.noreply.github.com>
2022-09-26 23:23:27 +0300

    modified:   anime-details.html 	modified:   categories.html 	modified:   index.html 	new file:   js/categoriesData.js 	new file:   js/detailData.js 	modified:   js/mainData.js

--- js/categoriesData.js
+++ js/categoriesData.js
@@ -1,4 +1,4 @@
-const mainData = () => {
+const categoriesData = () => {
     const preloader = document.querySelector('.preloder');
 
     const renderGanreList = (ganres) => {
@@ -12,11 +12,11 @@ const mainData = () => {
     }
 
     const renderAnimeList = (array, ganres) => {
-            const wrapper = document.querySelector('.product .col-lg-8');
+            const wrapper = document.querySelector('.product-page .col-lg-8');
             ganres.forEach((ganre) => {
             const productBlock = document.createElement('div')
             const listBlock = document.createElement('div')
-            const list = array.filter(item => item.ganre === ganre)
+            const list = array.filter(item => item.tags.includes(ganre))
 
             listBlock.classList.add('row')
             productBlock.classList.add('col-mb-5')
@@ -95,13 +95,19 @@ const mainData = () => {
     fetch('./db.json').then((response) => response.json())
     .then((data) => {
         const ganres = new Set()
+        const ganreParams = new URLSearchParams(window.location.search).get('ganre')
+        console.log(ganreParams)
         data.anime.forEach((item) => {
             ganres.add(item.ganre)
         })
         renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5))
-        renderAnimeList(data.anime, ganres)
+        if (ganreParams) {
+            renderAnimeList(data.anime, [ganreParams])
+        } else {
+            renderAnimeList(data.anime, ganres)
+        }
         renderGanreList(ganres)
     })
 }
 
-mainData();
+categoriesData();
